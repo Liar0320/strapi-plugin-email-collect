b59ab713707a046f29ce33cd013a3b52542c6c64
Li Chenghuan <100108350@qq.com>
2024-07-06 16:16:13 +0800

    chore: change Email Collect Dashboard Style

--- admin/src/pages/HomePage/index.tsx
+++ admin/src/pages/HomePage/index.tsx
@@ -4,15 +4,61 @@
  *
  */
 
-import React from 'react';
+import {
+  Box,
+  EmptyStateLayout,
+  Button,
+  Layout,
+  ContentLayout,
+  BaseHeaderLayout,
+  Typography,
+  GridLayout,
+  Divider
+} from "@strapi/design-system";
+import { Play, Cross, Plus, Eye } from "@strapi/icons";
+import { useIntl } from "react-intl";
+import { useHistory } from "react-router-dom";
+
+import React, { useState } from 'react';
 import pluginId from '../../pluginId';
+import getTrad from "../../utils/getTrad";
 
 const HomePage = () => {
+  const { formatMessage } = useIntl();
+  const [status, setStatus] = useState(false);
+  let history = useHistory();
+  function handleView() {
+    history.push(
+      `/content-manager/collection-types/plugin::email-collect.email-collect`
+    );
+  }
+
   return (
-    <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
-    </div>
+    <Layout background="primary100">
+    <BaseHeaderLayout
+      primaryAction={
+        <Button startIcon={<Eye />} onClick={handleView} >
+          View Entity
+        </Button>
+      }
+      title="Email Collect"
+      as="h2"
+    />
+
+    {status ? (
+      <ContentLayout>
+        <Box background="neutral100">
+        </Box>
+      </ContentLayout>
+    ) : (
+      <Box padding={8} background="neutral100">
+        <EmptyStateLayout
+          icon={<Cross />}
+          content="You don't have any content yet..."
+        />
+      </Box>
+    )}
+  </Layout>
   );
 };
 
